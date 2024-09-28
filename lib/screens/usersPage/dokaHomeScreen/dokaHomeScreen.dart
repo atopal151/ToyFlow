245cd5739c557db311ee2c8afaffb612b1676cbd
Alaettin TOPAL <atopal151@posta.pau.edu.tr>
2024-09-28 19:55:27 +0300

    19.55

---
+++ lib/screens/usersPage/dokaHomeScreen/dokaHomeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DokaHomeScreen extends StatelessWidget {
+  const DokaHomeScreen
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
