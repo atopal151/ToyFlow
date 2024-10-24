8847f8ab34485fe80eac7c031c97bb580be38477
Alaettin TOPAL <atopal151@posta.pau.edu.tr>
2024-10-24 10:20:43 +0300

    v106

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:toyflow/screens/SplashScreen/splash_screen.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Firebase başlat
+
   runApp(const MyApp());
 }
 
